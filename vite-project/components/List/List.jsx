346c2f61d23796e7b4337d7007b2f3b34d78f169
camila salvatierra <salvatierracm@gmail.com>
2023-08-10 18:35:32 -0300

    first commit

---
+++ vite-project/components/List/List.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const List = ({greeting}) =>{
+    return(
+        <div>
+            <h1>{greeting}</h1>
+        </div>
+
+    );
+};
+export default List;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import List from "../components/List/List";
+import NavBar from "../components/NavBar/NavBar";
+function App() {
+  
+  return (
+    
+  <div>
+    <NavBar/>
+    <List greeting="Hola, usuario!" />
+  </div>
+  );
+}
+
+export default App;
